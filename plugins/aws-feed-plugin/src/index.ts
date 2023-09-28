655e6460654b9e7424e53ad2e770e7b236d3bd74
Jason Bandy <jason.bandy77@gmail.com>
2023-09-28 11:15:55 -0500

    removing the aws plugin stuff

--- plugins/aws-feed-plugin/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { awsFeedPluginPlugin, AwsFeedPluginPage } from './plugin';
--- plugins/aws-feed-plugin/src/plugin.test.ts
+++
@@ -1,7 +0,0 @@
-import { awsFeedPluginPlugin } from './plugin';
-
-describe('aws-feed-plugin', () => {
-  it('should export plugin', () => {
-    expect(awsFeedPluginPlugin).toBeDefined();
-  });
-});
